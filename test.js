2f8020d9e8d0807c2f4de732985e4ee1878659a7
erobinofficial <robiul8778@gmail.com>
2022-10-16 17:34:30 +0600

    first commit

---
+++ test.js
@@ -0,0 +1,34 @@
+function clicked1() {
+    document.getElementById("border1").style.display = "none";
+    document.getElementById("border2").style.margin = '50px 200px 0px';
+    document.getElementById("border2").style.display = "block";
+}
+function clicked2() {
+    document.getElementById("border2").style.display = "none";
+    document.getElementById("border3").style.margin = '100px 20px 100px';
+    document.getElementById("border3").style.display = "block";
+}
+function clicked3() {
+    document.getElementById("border3").style.display = "none";
+    document.getElementById("border4").style.margin = '10px 80px 300px';
+    document.getElementById("border4").style.display = "block";
+}
+function clicked4() {
+    document.getElementById("border4").style.display = "none";
+    document.getElementById("border5").style.margin = '150px 200px 30px';
+    document.getElementById("border5").style.display = "block";
+}
+function clicked5() {
+    document.getElementById("border5").style.display = "none";
+    document.getElementById("border6").style.margin = '80px 100px 60px';
+    document.getElementById("border6").style.display = "block";
+}
+function clicked6() {
+    document.getElementById("border6").style.display = "none";
+    document.getElementById("border7").style.margin = '30px 62px 150px';
+    document.getElementById("border7").style.display = "block";
+}
+function clicked7() {
+    document.getElementById("border7").style.display = "none";
+    document.getElementById("divv").style.display = "block";
+}
